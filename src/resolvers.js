47cc5a48f096474a5aa4469a30ab1caea54a6d50
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-03-16 00:59:27 -0300

    feat: GET 1, GET N authors, e books. ADD book.

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,7 +3,9 @@ const { Book, Author } = require("./models")
 const resolvers = {
   Query: {
     hello: () => "Olá, mundo GraphQL",
+    author: async (_, { id }) => await Author.findById(id),
     authors: async() => await Author.find(),
+    book: async (_, { id }) => await Book.findById(id),
     books: async () => await Book.find(),
   },
   Mutation: {
@@ -18,6 +20,13 @@ const resolvers = {
       await author.save()
       return author
     }
+  },
+
+  Book: {
+    author: async (book) => await Author.findById(book.authorId)
+  },
+  Author: {
+    books: async (author) => await Book.find({ authorId: author.id })
   }
 }
 
